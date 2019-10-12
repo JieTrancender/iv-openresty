fde234b4be30df0f48c3ccfa520949f1be8a54fb
Jason <jie-email@jie-trancender.org>
2019-10-12 23:43:41 +0800

    add timestamp to notes

--- doc/mysql_db.sql
+++ doc/mysql_db.sql
@@ -3,3 +3,6 @@ create table `notes`
     `id` int(11) primary key auto_increment comment '主键id',
     `content` varchar(512) not null default '' comment '笔记内容'
 ) comment = '记录表';
+
+alter table `notes`
+add column createdTime TIMESTAMP(6) not NULL DEFAULT CURRENT_TIMESTAMP(6)
